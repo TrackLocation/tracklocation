d69b550e8878f54b7479de8ee9905d09d0428b92
I041936 <I041936@TLVD60211921A.sap.corp>
2013-10-30 13:45:41 +0200

    Logging infrastructure and permissions in manifest

--- TrackLocation/src/com/dagrest/tracklocation/MainActivity.java
+++ TrackLocation/src/com/dagrest/tracklocation/MainActivity.java
@@ -1,5 +1,7 @@
 package com.dagrest.tracklocation;
 
+import java.util.logging.LogManager;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
@@ -17,6 +19,9 @@ public class MainActivity extends Activity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
+		
+		LogManager.
+		
 		return true;
 	}
 
---
+++ TrackLocation/src/com/dagrest/tracklocation/log/MessageType.java
@@ -0,0 +1,5 @@
+package com.dagrest.tracklocation.log;
+
+enum MessageType   {          
+	FUNCTION_CALL, FUNCTION_EXIT, EXCEPTION, INFO, ERROR  
+}  
